2678782006d290377f7749fc2db9fd28c0fb9677
Rudraa <rudybbx3@gmail.com>
2024-08-21 20:13:54 +0400

    Added vercel link to CORS policy

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: ['http://localhost:5173','https://quiknote-app.vercel.app/'],
+  origin: ['http://localhost:5173','https://quiknote-app.vercel.app'],
   credentials: true,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify the allowed HTTP methods
   allowedHeaders: 'Content-Type,Authorization', // Specify the allowed headers
